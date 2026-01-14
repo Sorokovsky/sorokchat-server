b8596a16bbf20bb7062823daa7b780105e7359af
Sorokovskys <Sorokovskys@ukr.net>
2026-01-14 14:13:09 +0200

    start

---
+++ src/main/java/com/sorokovsky/sorokchat/SorokchatApplication.java
@@ -0,0 +1,13 @@
+package com.sorokovsky.sorokchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorokchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorokchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sorokovsky/sorokchat/SorokchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorokovsky.sorokchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorokchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
