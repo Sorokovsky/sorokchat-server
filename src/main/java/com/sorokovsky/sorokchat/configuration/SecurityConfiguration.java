b6ce4bfb834ac6725ae931685c41971d8e45b6c4
Sorokovskys <Sorokovskys@ukr.net>
2026-01-15 14:49:08 +0200

    AuthorizationController.java

--- src/main/java/com/sorokovsky/sorokchat/configuration/SecurityConfiguration.java
+++ src/main/java/com/sorokovsky/sorokchat/configuration/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.sorokovsky.sorokchat.configuration;
 
 import com.sorokovsky.sorokchat.factory.DefaultAccessTokenFactory;
 import com.sorokovsky.sorokchat.factory.DefaultRefreshTokenFactory;
+import com.sorokovsky.sorokchat.model.Authority;
 import com.sorokovsky.sorokchat.service.AuthorizationService;
 import com.sorokovsky.sorokchat.service.UsersService;
 import com.sorokovsky.sorokchat.storage.BearerTokenStorage;
@@ -9,7 +10,9 @@ import com.sorokovsky.sorokchat.storage.CookieTokenStorage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,14 +24,19 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(configurer -> configurer
                         .requestMatchers("/swagger-ui/**", "/openapi.yaml", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/authorization/login", "/authorization/register").anonymous()
+                        .requestMatchers("/authorization/logout").hasAuthority(Authority.JWT_LOGOUT.name())
                         .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(configurer -> configurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
