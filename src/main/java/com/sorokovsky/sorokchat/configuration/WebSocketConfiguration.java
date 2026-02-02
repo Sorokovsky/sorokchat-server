5e5da5c12588b4e5254e99c29987d81c39e3301b
Sorokovsky <Sorokovskys@ukr.net>
2026-02-02 17:24:22 +0200

    fix: websocket config

--- src/main/java/com/sorokovsky/sorokchat/configuration/WebSocketConfiguration.java
+++ src/main/java/com/sorokovsky/sorokchat/configuration/WebSocketConfiguration.java
@@ -5,16 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.security.authorization.AuthorizationManager;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
-import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -26,7 +20,6 @@ import java.util.Map;
 
 @Configuration
 @EnableWebSocketMessageBroker
-@EnableWebSocketSecurity
 @RequiredArgsConstructor
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketConfiguration.class);
@@ -66,15 +59,5 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     public void configureClientInboundChannel(ChannelRegistration registration) {
         LOGGER.info("configureClientInboundChannel викликано");
         registration.interceptors(jwtChannelInterceptor);
-    }
-
-    @Bean
-    public AuthorizationManager<Message<?>> messageAuthorizationManager(
-            MessageMatcherDelegatingAuthorizationManager.Builder messages
-    ) {
-        messages
-                .simpTypeMatchers(SimpMessageType.CONNECT, SimpMessageType.DISCONNECT, SimpMessageType.HEARTBEAT).permitAll()
-                .simpDestMatchers("/app/**").authenticated();
-        return messages.build();
     }
 }
