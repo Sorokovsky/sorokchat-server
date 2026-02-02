544e0da9cd6cd895f28dbe0da2ac62a35cc81f06
Sorokovsky <Sorokovskys@ukr.net>
2026-02-02 13:33:46 +0200

    fix: websocket config

--- src/main/java/com/sorokovsky/sorokchat/configuration/WebSocketConfiguration.java
+++ src/main/java/com/sorokovsky/sorokchat/configuration/WebSocketConfiguration.java
@@ -2,13 +2,17 @@ package com.sorokovsky.sorokchat.configuration;
 
 import com.sorokovsky.sorokchat.interceptors.JwtChannelInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.simp.config.ChannelRegistration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -16,12 +20,6 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @RequiredArgsConstructor
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     private final JwtChannelInterceptor jwtChannelInterceptor;
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -32,9 +30,21 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     }
 
     @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+
+    }
+
+    @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(jwtChannelInterceptor);
     }
 
-
+    @Bean
+    public AuthorizationManager<Message<?>> messageAuthorizationManager(
+            MessageMatcherDelegatingAuthorizationManager.Builder messages
+    ) {
+        messages
+                .anyMessage().authenticated();
+        return messages.build();
+    }
 }
